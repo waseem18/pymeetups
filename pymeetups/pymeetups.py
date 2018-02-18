5bbd5028dbd4212ed52b600df5586b1f593cb9a3
Wasim Thabraze <wasim@thabraze.me>
2018-02-19 00:33:29 +0530

    Added one more calendar id and moved from tabulate to texttable for better table formatting

--- pymeetups/pymeetups.py
+++ pymeetups/pymeetups.py
@@ -1,13 +1,14 @@
 from google.oauth2 import service_account
 import googleapiclient.discovery
 import datetime
-import tabulate
+import texttable
 import os
 
 
 class PyMeetups:
     def __init__(self):
         self.calendar_id = "j7gov1cmnqr9tvg14k621j7t5c@group.calendar.google.com"
+        self.calendar_id_groups = "3haig2m9msslkpf2tn1h56nn9g@group.calendar.google.com"
         self.SERVICE_ACCOUNT_FILE = ("{}/project.json".format(os.path.dirname(os.path.realpath(__file__))))
         self.SCOPES = [
             'https://www.googleapis.com/auth/calendar',
@@ -15,7 +16,7 @@ class PyMeetups:
         ]
         self.time_min = datetime.datetime.now().strftime('%Y-%m-%dT00:00:00.000Z')
 
-    def fetch_events_from_calendar(self):
+    def fetch_events_from_calendar(self, cal_id):
         page_token = None
         credentials = service_account.Credentials.from_service_account_file(
             self.SERVICE_ACCOUNT_FILE, scopes=self.SCOPES)
@@ -23,7 +24,7 @@ class PyMeetups:
         events = []
         while True:
             response = calendar.events().list(
-                calendarId=self.calendar_id, timeMin=self.time_min, pageToken=page_token
+                calendarId=cal_id, timeMin=self.time_min, pageToken=page_token
             ).execute()
             events.extend([event for event in response['items']])
             page_token = response.get('nextPageToken')
@@ -39,7 +40,7 @@ class PyMeetups:
             for key, value in event.items():
                 if key in headers_to_show:
                     if key == headers_to_show[2]:
-                        d[key] = value['date']
+                        d[key] = value.get('dateTime', value.get('date'))
                     else:
                         d[key] = value
 
@@ -47,9 +48,15 @@ class PyMeetups:
                 filtered_events.append(d)
         return filtered_events
 
-    def populate_tabulate(self):
-        events = self.fetch_events_from_calendar()
+    def populate_texttable(self):
+        events1 = self.fetch_events_from_calendar(self.calendar_id)
+        events2 = self.fetch_events_from_calendar(self.calendar_id_groups)
+        events = events1 + events2
         filtered_events = self.filter_events(events)
-        headers = filtered_events[0].keys()
         rows = [i.values() for i in filtered_events]
-        print(tabulate.tabulate(rows,headers, tablefmt="rst"))
+        table = texttable.Texttable()
+        table.header(filtered_events[0].keys())
+        table.set_cols_align(["l", "r", "c"])
+        table.set_cols_valign(["t", "m", "b"])
+        table.add_rows(rows,header=False)
+        print(table.draw())
